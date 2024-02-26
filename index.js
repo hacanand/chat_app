f76aea0b21d09c5a59f4a0473d669a24f545c7d2
anand <anandpromax@gmail.com>
2024-02-26 19:12:30 +0530

    'new message'

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+
+const socketIo = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+    socket.on('msg_send', (data) => {
+        console.log(data);
+  })
+ 
+});
+app.use("/", express.static(__dirname + "/public"));
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
